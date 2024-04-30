e56ae3c333aa70c598361483887bd7b2a3baf2bf
Tijani <ayoub-tijani456@hotmail.com>
2024-05-01 00:18:45 +0100

    initi project Add

---
+++ src/test/java/com/stgProject/learn/spring5/LearnSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stgProject.learn.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
