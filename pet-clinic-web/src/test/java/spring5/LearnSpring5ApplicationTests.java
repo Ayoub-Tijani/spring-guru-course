e82e53453585aa61be15d5914ccf0c9081c92ce3
Tijani <ayoub-tijani456@hotmail.com>
2024-05-11 03:43:59 +0100

    refactoring multi-module maven project

--- pet-clinic-web/src/test/java/spring5/LearnSpring5ApplicationTests.java
+++ pet-clinic-web/src/test/java/spring5/LearnSpring5ApplicationTests.java
@@ -1,9 +1,12 @@
 package spring5;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
 class LearnSpring5ApplicationTests {
 
 	@Test
